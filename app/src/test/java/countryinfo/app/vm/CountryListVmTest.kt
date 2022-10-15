1fef1eeeaa5e43c220acd6a5944e8fb689bd90d7
swapnil <swapnil.todkar@tcs.com>
2022-10-15 13:01:20 +0530

    Added test for api call repo

--- app/src/test/java/countryinfo/app/vm/CountryListVmTest.kt
+++ app/src/test/java/countryinfo/app/vm/CountryListVmTest.kt
@@ -3,9 +3,11 @@ package countryinfo.app.vm
 import countryinfo.app.api.ApiInterface
 import countryinfo.app.repo.CountryListRepo
 import io.mockk.mockk
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -16,11 +18,13 @@ internal class CountryListVmTest {
     val mockApiInterface = mockk<ApiInterface>()
 
     val repo: CountryListRepo = mockk(relaxed = true)
-    val vm: CountryListVm = CountryListVm(repo, StandardTestDispatcher())
+    lateinit var vm: CountryListVm
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
-
+        Dispatchers.setMain(StandardTestDispatcher())
+        vm = CountryListVm(repo, StandardTestDispatcher())
     }
 
     @After
@@ -29,8 +33,7 @@ internal class CountryListVmTest {
 
     @Test
     fun getCountryList() = runTest {
-        vm.getCountryList()
-        vm.observeCountryList().collectLatest { }
+
 
     }
 
