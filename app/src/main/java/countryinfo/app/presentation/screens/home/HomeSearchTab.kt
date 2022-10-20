91a6c5874e12000b1c82ff0d1e149a95df2f8975
2463489 <2463489@tcs.com>
2022-10-20 19:51:43 +0530

    -Create common component for Search Bar -handle Search bar Api Extra request by Viewmodel

--- app/src/main/java/countryinfo/app/presentation/screens/home/HomeSearchTab.kt
+++ app/src/main/java/countryinfo/app/presentation/screens/home/HomeSearchTab.kt
@@ -1,35 +1,18 @@
 package countryinfo.app.presentation.screens.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.SettingsVoice
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import countryinfo.app.R
 import countryinfo.app.presentation.vm.CountryListVm
-import countryinfo.app.theme.SearchBG
 import countryinfo.app.uicomponents.CountryListView
-import countryinfo.app.uicomponents.scaffold_comp.getDP
+import countryinfo.app.uicomponents.DsgSearchComponent
 import countryinfo.app.utils.*
 import countryinfo.app.utils.networkconnection.ConnectionState
 import countryinfo.app.utils.networkconnection.connectivityState
@@ -40,6 +23,7 @@ fun HomeSearchTab(navController: NavController?, viewModel: CountryListVm) {
     val countryList = viewModel.observeCountryList().collectAsState()
     val searchList = viewModel.observeSearchCountryList().collectAsState()
     val errorState = viewModel.observeErrorState().collectAsState()
+
 
     val query = viewModel.searchQuery().collectAsState().value
 
@@ -52,7 +36,18 @@ fun HomeSearchTab(navController: NavController?, viewModel: CountryListVm) {
         }
     }
 
-        viewModel.title.value = titleSearch
+    LaunchedEffect(key1 = query) {
+
+    if(query!=viewModel.lastSearchItem.value) {
+
+         viewModel.searchByDebounce(query)
+    }
+        if (query.isEmpty()) {
+            viewModel.clearSearch()
+        }
+    }
+
+    viewModel.title.value = titleSearch
 
     Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
         Column(
@@ -61,7 +56,11 @@ fun HomeSearchTab(navController: NavController?, viewModel: CountryListVm) {
                 .padding()
         ) {
 
-            SearchTextField(viewModel)
+            DsgSearchComponent(query =query, onValueChange = {
+                viewModel.updateSearchQuery(it)
+            } ) {
+                viewModel.convertSpeechToText()
+            }
 
             if (searchList.value.isEmpty() && query.length < 2) {
                 CountryListView(true, isConnected, errorState, navController, countryList.value) {
@@ -81,66 +80,6 @@ fun HomeSearchTab(navController: NavController?, viewModel: CountryListVm) {
     }
 
 
-}
-
-@Composable
-fun SearchTextField(viewModel: CountryListVm) {
-    val query = viewModel.searchQuery().collectAsState().value
-    var isVoicePermissionGranted = checkRecordAudioPermission()
-    LaunchedEffect(key1 = query) {
-        viewModel.searchByDebounce(query)
-        if (query.isEmpty()) {
-            viewModel.clearSearch()
-        }
-    }
-
-    TextField(
-        value = query,
-        onValueChange = {
-            viewModel.updateSearchQuery(it)
-        },
-        placeholder = {
-            Text(text = stringResource(id = R.string.search),
-            fontWeight = FontWeight.Normal,color = Color.Black)},
-        modifier = Modifier
-            .testTag("country_search_text_field")
-            .padding(all = getDP(dimenKey = R.dimen.dp_8))
-            .fillMaxWidth()
-            .background(SearchBG)
-            .border(
-                width = getDP(dimenKey = R.dimen.dp_8),
-                color = Color.White,
-                shape = RoundedCornerShape(getDP(dimenKey = R.dimen.dp_20))
-            ),
-        singleLine = true,
-        textStyle = TextStyle(fontSize = 16.sp, color = Color.Black),
-        leadingIcon = { Icon(Icons.Default.Search, contentDescription = EMPTY_STRING) },
-        trailingIcon = {
-            if (isVoicePermissionGranted) {
-                IconButton(
-                    onClick = {
-                        if (isVoicePermissionGranted) {
-                            viewModel.convertSpeechToText()
-                        }
-                    },
-                ) {
-                    Icon(
-                        Icons.Default.SettingsVoice,
-                        contentDescription = EMPTY_STRING,
-                        tint = Color.Gray
-                    )
-                }
-            } else {
-                null
-            }
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            cursorColor = Color.Gray
-        ),
-        shape = RoundedCornerShape(getDP(dimenKey = R.dimen.dp_20))
-    )
 }
 
 @Preview(showBackground = true)
